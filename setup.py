b9762412d842cb056265b0e7438616a3b6256e33
Rohan Vahalia <r.vahalia@gmail.com>
2020-03-04 16:32:58 -0600

    Add requirements for version 1

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="YahooScrapingTools",
     version='1.0',
     packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'objectpath', 'pytz', 'yahoo_oauth', 'docopt', 'yahoo_fantasy_api'
+    ],
+    python_requires='>=3',
 )
